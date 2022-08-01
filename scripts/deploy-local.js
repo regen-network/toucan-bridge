ab50d25b746fceba4d83b0d6dde4e1f8a8d16b36
Dave Brophy <dave@brophy.uk>
2022-08-01 11:43:48 -0300

    Tweak scripts to support multiple token owners and transfers

--- scripts/deploy-local.js
+++ scripts/deploy-local.js
@@ -4,21 +4,21 @@ const { deployBridge } = require("../lib/bridge");
 const { prepareToucanEnv } = require("../lib/toucan");
 
 async function deploy() {
-	const [owner] = await hre.ethers.getSigners();
-	const regenBridgeAccount = "0x11241e35B3f79099123aA0C1C4c97b1FcdCd21f6";
-	const { registry, tco2Factory, nctPool, data } = await prepareToucanEnv(owner, owner);
+	const [adminAccount, bridgeAccount] = await hre.ethers.getSigners();
+	const { registry, tco2Factory, nctPool, data } = await prepareToucanEnv(adminAccount, null);
 	let dataAddresses = {};
 	for (const key in data) {
 		dataAddresses[key] = data[key].address;
 	}
-	const bridge = await deployBridge(regenBridgeAccount, registry.address, nctPool.address);
+	const bridge = await deployBridge(bridgeAccount.address, registry.address, nctPool.address);
 	console.log(`Adding bridge contract address ${bridge.address} to allow list...`);
 	await tco2Factory.addToAllowlist(bridge.address);
 	console.log("==== config data ====");
 	console.log(
 		JSON.stringify(
 			{
-				OWNER: owner.address,
+				ADMIN_ACCOUNT: adminAccount.address,
+				BRIDGE_ACCOUNT: bridgeAccount.address,
 				BRIDGE_CONTRACT_ADDRESS: bridge.address,
 				TCO2_FACTORY_ADDRESS: tco2Factory.address,
 				REGISTRY_ADDRESS: registry.address,
