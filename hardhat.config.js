0477159925408723551f5df62a9d73df001a3a3d
pheuberger <philipp@pheuberger.com>
2022-09-05 18:03:01 +0200

    chore: remove cruft from hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -37,19 +37,6 @@ if (PRIVATE_KEY) {
 	};
 }
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-	const accounts = await hre.ethers.getSigners();
-
-	for (const account of accounts) {
-		console.log(account.address);
-	}
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 module.exports = {
 	solidity: {
 		version: "0.8.4",
@@ -61,13 +48,6 @@ module.exports = {
 		},
 	},
 	networks: {
-		// Hardhat comes built-in with a special network called hardhat. When using this network, an instance of the Hardhat Network will be automatically created when you run a task, script or test your smart contracts.
-		// default networks: hardhat, localhost ("http://127.0.0.1:8545")
-		// rinkeby: {
-		//   // url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-		//   // url: "https://rinkeby.infura.io/v3/",
-		//   // accounts: [``]
-		// }
 		hardhat: {
 			// See: https://github.com/regen-network/toucan-bridge/issues/37
 			allowUnlimitedContractSize: true,
