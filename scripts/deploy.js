e3e448cd39f78a1b1bd14ec0032c46129ac53657
Michalis Kargakis <kargakis@protonmail.com>
2023-03-06 14:05:13 +0100

    Update deploy scripts after latest constructor arg updates

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,27 +1,29 @@
-const assert = require("assert");
-const fs = require("fs");
 const hre = require("hardhat");
 
+const argumentsMatic = require("./arguments-matic");
+const argumentsMumbai = require("./arguments-mumbai");
 const { deployBridge } = require("../lib/bridge");
 
-const { MNEMONIC, BRIDGE_CONTROLLER_ADDRESS, NCT_ADDRESS } = process.env;
-
 async function deploy() {
-	assert.ok(NCT_ADDRESS, "NCT_ADDRESS environment variable must be set");
-	// Set minter to empty if not provided
-	let tokenIssuer = BRIDGE_CONTROLLER_ADDRESS;
-	if (!tokenIssuer) {
-		tokenIssuer = hre.ethers.constants.AddressZero;
+	const [deployer] = await hre.ethers.getSigners();
+	const args = [];
+	if (hre.network.name === "matic") {
+		args.push(...argumentsMatic);
+	} else if (hre.network.name === "mumbai") {
+		args.push(...argumentsMumbai);
+	} else {
+		throw Error(
+			`trying to deploy in network ${hre.network.name}. Need to define network arguments in "./arguments-${hre.network.name}" and import the new file in the deploy script`
+		);
 	}
 
-	const [deployer] = await hre.ethers.getSigners();
+	console.log(`Deployer: ${deployer.address}`);
+	const [nctPool, roles, accounts] = args;
+	console.log(`NCT pool: ${nctPool}`);
+	console.log(`Roles:    ${roles}`);
+	console.log(`Accounts: ${accounts}`);
 
-	console.log(`Deploying bridge contract with the following addresses:`);
-	console.log(`Owner: ${deployer.address}`);
-	console.log(`Token Issuer: ${tokenIssuer}`);
-	console.log(`NCT Pool: ${NCT_ADDRESS}`);
-
-	const bridge = await deployBridge(tokenIssuer, NCT_ADDRESS);
+	const bridge = await deployBridge(deployer, ...args);
 
 	console.log(`Deployed bridge with address ${bridge.address}`);
 	console.log("Remember to add bridge address to the allow list");
